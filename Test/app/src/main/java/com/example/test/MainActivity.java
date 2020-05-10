32be29643cb00eb0f6f0dc3f69748312b39f6c12
miaotengfei6 <1101589983@qq.com>
2020-05-10 18:45:38 +0800

    mtf

---
+++ Test/app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private char[][] chars={{1,1,1,1,0},{1,1,0,1,0},{1,1,0,0,0},{0,0,0,0,0}};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        numIslands(chars);
+    }
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) return 0;
+        int row = grid.length, columns = grid[0].length, count = 0;
+        for (int i = 0; i < row; i++) {//遍历所有点
+            for (int j = 0; j < columns; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j, row, columns);//dfs遍历所有连接的点
+                    count++;//记录岛屿数量
+                }
+            }
+        }
+        return count;
+    }
+
+    private void dfs(char[][] grid, int i, int j, int row, int columns) {
+        if (i < 0 || j < 0 || i >= row || j >= columns || grid[i][j] == '0') return;//基线条件
+        grid[i][j] = '0';//遍历过的点置 0
+        dfs(grid, i + 1, j, row, columns);
+        dfs(grid, i, j + 1, row, columns);
+        dfs(grid, i - 1, j, row, columns);
+        dfs(grid, i, j - 1, row, columns);
+    }
+
+}
